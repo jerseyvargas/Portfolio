82a74a30127c2195227deada434b5fccdd056227
Jersey Vargas <jerseyvargas26@gmail.com>
2025-07-24 09:00:21 -0700

    initial commit p2

---
+++ TrueVoiceScript.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const icons = document.querySelectorAll('.TV_constraint_item');
+    const texts = document.querySelectorAll('.constraint_text');
+
+    icons.forEach((icon, index) => {
+        icon.addEventListener('mouseover', () => {
+            texts.forEach((txt, i) => {
+                if (i === index) {
+                    txt.classList.toggle('show');
+                } else {
+                    txt.classList.remove('show');
+                }
+            });
+        });
+    });
+});
